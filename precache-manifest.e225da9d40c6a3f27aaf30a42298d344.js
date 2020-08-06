f0733a89eb876cd444f990f12b6e279d78594769
70NY <53057036+TonyGLL@users.noreply.github.com>
2020-08-06 11:50:19 -0500

    Updates

---
+++ precache-manifest.e225da9d40c6a3f27aaf30a42298d344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec4879b5a52d2701039fd6f2883daf3",
+    "url": "/movies-app-reactjs/index.html"
+  },
+  {
+    "revision": "158a1879eeba558a5b2f",
+    "url": "/movies-app-reactjs/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "f313fda74f8562b63049",
+    "url": "/movies-app-reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "158a1879eeba558a5b2f",
+    "url": "/movies-app-reactjs/static/js/2.51f9906f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-app-reactjs/static/js/2.51f9906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f313fda74f8562b63049",
+    "url": "/movies-app-reactjs/static/js/main.16fcac0e.chunk.js"
+  },
+  {
+    "revision": "0d82d5d101540f74ffcd",
+    "url": "/movies-app-reactjs/static/js/runtime-main.387d744e.js"
+  }
+]);
